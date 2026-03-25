713e83062f8706eeafb5b1eca8f12facafe8813b
Eruumaa <maqilmb30@gmail.com>
2026-03-25 08:56:03 +0700

    queueuarr done

--- queueassignment/queuearr.c
+++ queueassignment/queuearr.c
@@ -7,20 +7,51 @@ void initQueueArr (QueueArr *queue) {
     queue->count = 0;
 }
 
+int isEmptyArr(QueueArr *queue) {
+    return (queue->count == 0);
+}
+
 int isFullArr(QueueArr *queue) {
     return (queue->count == MAX);
 }
 
 void enqueueArr(QueueArr *queue, int value) {
     if (isFullArr(queue)) {
-        printf("Antrian penuh\n");
+        printf("Queue penuh\n");
         return;
     }
     queue->rear = (queue->rear + 1) % MAX;
     queue->data[queue->rear] = value;
     queue->count++;
 }
-void displayArr(QueueArr *queue);
-int isFullArr(QueueArr *queue);
-int dequeueArr(QueueArr *queue);
-int peekArr(QueueArr *queue);
+
+int dequeueArr(QueueArr *queue) {
+    if (isEmptyArr(queue)) {
+        return -1;
+    }
+    int value = queue->data[queue->front];
+    queue->front = (queue->front + 1) % MAX;
+    queue->count--;
+    return value;
+}
+
+int peekArr(QueueArr *queue) {
+    if (isEmptyArr(queue)) {
+        return -1;
+    }
+    return queue->data[queue->front];
+}
+
+void displayArr(QueueArr *queue) {
+    if (isEmptyArr(queue)) {
+        printf("Queue Kosong\n");
+        return;
+    }
+    printf("Isi Queue: ");
+    int i, index = queue->front;
+    for (i = 0; i < queue->count; i++) {
+        printf("[%d] ", queue->data[index]);
+        index = (index + 1) % MAX;
+    }
+    printf("\n");
+}
