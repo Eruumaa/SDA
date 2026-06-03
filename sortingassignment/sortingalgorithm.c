c2cec7e7d7cec459c723d6de9a4f63601d786248
Eruumaa <maqilmb30@gmail.com>
2026-06-03 11:05:55 +0700

    adding insertion sort implementation

--- sortingassignment/sortingalgorithm.c
+++ sortingassignment/sortingalgorithm.c
@@ -74,8 +74,62 @@ Sortstats insertionSortInt (int arr[], int n) {
 }
 
 // Merge Sort
-Sortstats mergeSortInt (int arr[], int n) {
+void mergeint (int arr[], int left, int right, Sortstats *stats) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        mergeint (arr, left, mid, stats);
+        mergeint (arr, mid + 1, right, stats);
+        
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        
+        int *leftArr =  (int *)malloc(n1 * sizeof(*leftArr));
+        int *rightArr =  (int *)malloc(n2 * sizeof(*rightArr));
+        
+        for (int i = 0; i < n1; i++) leftArr[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) rightArr[j] =arr[mid + 1 + j];
+        
+        int i = 0, j = 0, k = left;
+        
+        while (i < n1 && j < n2) {
+            stats->comparison++; 
+            if (strcmp(leftArr[i], rightArr[j]) <= 0) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            stats->swap++; 
+            k++;
+        }
+        
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            stats->swap++;
+            i++; k++;
+        }
+        
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            stats->swap++;
+            j++; k++;
+        }
+        
+        free(leftArr);
+        free(rightArr);
+    }
+}
 
+Sortstats mergeSortInt (int arr[], int n) {
+    Sortstats stats = {0,0, 0.0};
+    clock_t start = clock();
+
+    mergeint(arr, 0, n-1, &stats);
+
+    stats.timeTaken = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+    return stats;
 }
 
 // Quick Sort
@@ -99,9 +153,63 @@ Sortstats insertionSortStr (char arr[][100], int n) {
 
 }
 
+void mergeStr (char arr[][100], int left, int right, Sortstats *stats) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        mergeStr (arr, left, mid, stats);
+        mergeStr (arr, mid + 1, right, stats);
+        
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        
+        char (*leftArr)[100] = malloc(n1 * sizeof(*leftArr));
+        char (*rightArr)[100] = malloc(n2 * sizeof(*rightArr));
+        
+        for (int i = 0; i < n1; i++) strcpy(leftArr[i], arr[left + i]);
+        for (int j = 0; j < n2; j++) strcpy(rightArr[j], arr[mid + 1 + j]);
+        
+        int i = 0, j = 0, k = left;
+        
+        while (i < n1 && j < n2) {
+            stats->comparison++; 
+            if (strcmp(leftArr[i], rightArr[j]) <= 0) {
+                strcpy(arr[k], leftArr[i]);
+                i++;
+            } else {
+                strcpy(arr[k], rightArr[j]);
+                j++;
+            }
+            stats->swap++; 
+            k++;
+        }
+        
+        while (i < n1) {
+            strcpy(arr[k], leftArr[i]);
+            stats->swap++;
+            i++; k++;
+        }
+        
+        while (j < n2) {
+            strcpy(arr[k], rightArr[j]);
+            stats->swap++;
+            j++; k++;
+        }
+        
+        free(leftArr);
+        free(rightArr);
+    }
+}
+
 // Merge Sort
 Sortstats mergeSortStr (char arr[][100], int n) {
+        Sortstats stats = {0,0, 0.0};
+    clock_t start = clock();
 
+    mergeStr(arr, 0, n - 1, &stats);
+
+    stats.timeTaken = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+    return stats;
 }
 
 // Quick Sort
