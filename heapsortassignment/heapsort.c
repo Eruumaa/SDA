48760aa39b1065a19d75c16e2f1f721c515b936d
Eruumaa <maqilmb30@gmail.com>
2026-06-09 10:38:04 +0700

    adding heap functions

--- heapsortassignment/heapsort.c
+++ heapsortassignment/heapsort.c
@@ -1,3 +1,4 @@
 #include "heapsort.h"
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+
---
+++ heapsortassignment/heapsort.h
@@ -0,0 +1,4 @@
+void minHeapify(double arr[], int , int , long long *);
+void maxHeapifyString(char arr[][100], int , int , long long *);
+void heapSortDesc (double arr[], int , long long *);
+void heapSortString (char arr[][100], int , long long *);
